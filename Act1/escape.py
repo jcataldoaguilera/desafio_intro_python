2fb23de8e47f973a546338bc73bce193265f8d0a
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-11 18:26:22 -0300

    se agregan comentarios

--- Act1/escape.py
+++ Act1/escape.py
@@ -10,11 +10,13 @@ from math import sqrt
 # Definicion y reciclaje de Variables
 radio = input("Ingrese el radio en Kilometros: ")
 cons_g = input("Ingrese la constante g: ")
+# Definimos data type de las variables
 radio = int(radio)
 cons_g = float(cons_g)
 # A nivel de formula, el radio se expresa en metros, pero en el input se nos solicita ingresar el radio en Kilometros,
 # por lo que multiplicamos el input por 1000 para transformar la unidad.
 escape = sqrt(2*cons_g*(1000*radio))
+# Redondeamos decimales
 escape = round(escape,2)
 
 # Imprimir resultados
