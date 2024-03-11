8f09d54c1306d0623f6e54f9bf192759ab0d4976
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-11 19:48:28 -0300

    se entegan programas solicitados

---
+++ Act2/emprendedor3.py
@@ -0,0 +1,29 @@
+# Info
+# Autor: Juan Cataldo [jcataldoaguilera@gmail.com]
+# Fecha: 2024-03-11
+# RLAB-23-02-09-0044-2
+#
+
+# Advertencia
+print("!!! Los valores ingresados deben ser enteros y sin separador de millares ¡¡¡")
+
+# Definicion de variables
+P = input("Ingrese precio de suscripción $: ")
+U = input("Ingrese número de usuarios: ")
+GT = input("Ingrese el gasto total $: ")
+Upas = input("Ingrese las utilidades del año anterior $: ")
+
+# Definimos el data type
+P = int(P)
+U = int(U)
+GT = int(GT)
+Upas = int(Upas)
+
+# Calculamos
+UT = P*U-GT
+Ur = UT/Upas
+Ur = round(Ur,2)
+
+# Imprimir resultados
+print(f"La rentabilidad es: ${UT}")
+print(f"La razón entre las utilidades acutales y las del año anterior es {Ur}")
